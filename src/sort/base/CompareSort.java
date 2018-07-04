e931547c1e76fc254dbacda261b90fbf1973018e
leaderHoo <1396894821@qq.com>
2018-07-04 22:00:02 +0800

    fixBugs

--- src/sort/base/CompareSort.java
+++ src/sort/base/CompareSort.java
@@ -11,6 +11,9 @@ import java.util.List;
  * @Date: 2018/6/29 11:40
  * @desc: 比较各种排序算法的执行时间
  *        测试算法执行时间时，第一个问题：传入的数组长度是 new Integer[10000],但是可能 a[20]为null
+ * @apiNote
+ *      排序之前，将数组打乱，避免出现最坏情况
+ *      注意力放到重复元素，
  */
 public class CompareSort {
 
--- src/sort/quickSort/Quick3Way.java
+++ src/sort/quickSort/Quick3Way.java
@@ -6,6 +6,11 @@ import sort.base.Basetemplate;
  * @Author: leaderHoo
  * @Date: 2018/7/3 17:26
  * @Desc： 三项切分快速排序
+ *     每次切分之后，位于gt指针和lt指针之间的元素的位置都已经被排定，不需要再去处理了。
+ *          之后将（lo,lt-1）,（gt+1,hi）分别作为处理左子数组和右子数组的递归函数的参数传入
+ *
+ *    维护 三个指针 lt  i  gt  如果 a[i] < temp  , exch(a,i++,lt++) ; 如果 a[i] > temp  exch(a,i,gt--) ;
+ *          如果相同 i++ ; i小于gt一直循环</>
  */
 public class Quick3Way extends Basetemplate {
 
@@ -15,12 +20,13 @@ public class Quick3Way extends Basetemplate {
             return;
         sort(a,0,a.length-1);
     }
-    //三项切分
+    //三项切分 ：每次递归都保证 lt - gt 是有序的。
     private void sort(Comparable[] a, int left, int right) {
         if (left >= right)
             return;
         int lt = left,gt=right,i=left+1;
         Comparable v = a[left];
+        // 切分成为 小于a[i] + 等于a[i] + 大于a[i] 。a[left]- a[i] ; a [i] - a[gt] ; a[gt]-a[right]
         while (i <= gt){
             int cmp = a[i].compareTo(v);
             if (cmp < 0) exch(a,lt++,i++);
