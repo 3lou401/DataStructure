1cabc935afee1e992ca525338b0751a2319235f2
leaderHoo <1396894821@qq.com>
2018-07-06 09:37:42 +0800

    add priorityqueue module

--- src/Heap/PriorityQueue/PriorityQueueByLinkedList.java
+++ src/Heap/PriorityQueue/PriorityQueueByLinkedList.java
@@ -24,7 +24,11 @@ public class PriorityQueueByLinkedList <key extends Comparable<key>>
     }
 
     private Link first;//新的头节点
+    private Link current;//保存中间节点信息，保证节点在移动过程中不丢失上一个节点信息
+    private Link previous;//旧的头节点
+    private boolean flag = false;
     private int size = 0;//链表的长度
+
 
     public PriorityQueueByLinkedList() {
         this.first = null;
@@ -36,9 +40,31 @@ public class PriorityQueueByLinkedList <key extends Comparable<key>>
     }
 
     @Override
-    void add(key key) {
+    void add(key k) {
+        /*把数据插入到链表的第一个节点*/
         Link link = new Link();
+        link.priority = (key) k;
+        link.next = first;
+        first = link;
 
+        current = first;
+        previous = first.next;
+
+        /*得到数据插入的位置，flag用来控制是否要对节点移动*/
+        while (current.next != null && link.priority.compareTo(current.next.priority) >= 0)  {
+            flag = true;
+            if(current.next == null){break;}
+            current = current.next;
+        }
+
+        /*移动节点到目标位置*/
+        if(flag)  {
+            link.next = current.next;
+            current.next = link;
+            first = previous;
+            flag  = false;
+        }
+        size++;
     }
 
     @Override
